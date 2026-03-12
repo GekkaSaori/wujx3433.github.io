4c0af365b9a4cf64f3f56579e431a9e2f87cbf66
GekkaSaori <gekka.saori@yandex.com>
2026-03-12 20:24:05 +0800

    fix: remove extra space in replace list.

--- .vitepress/components/Vertical.vue
+++ .vitepress/components/Vertical.vue
@@ -28,7 +28,7 @@ function doVerticalAdjustments() {
   
   // 横排引号转换为竖排引号
   var divList = ['p', 'h1', 'h2', 'h3', 'h4', 'h5']
-  var replaceList = ['“', '『', '”', '』', '‘ ', '「', '’', '」'];
+  var replaceList = ['“', '『', '”', '』', '‘', '「', '’', '」'];
   for(var i = 0; i < divList.length; ++i) {
       var selectedDiv = document.querySelector('.vertical-layout-container')!.querySelectorAll(divList[i]);
       for(var j = 0; j < selectedDiv.length; ++j) {
